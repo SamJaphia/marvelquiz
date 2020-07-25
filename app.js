07ffa0a33e944adad16cc65d43953e692f94edb4
Japhia <smante@hotmail.co.uk>
2020-07-25 22:51:05 +0100

    Second update

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ import { publicKey, privateKey } from './secrets.mjs';
 let timestamp = Date.now();
 let md5Hash = md5(`${timestamp}${privateKey}${publicKey}`);
 
-fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
+fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&modifiedSince=2018-03-01&limit=50?&apikey=${publicKey}&hash=${md5Hash}`)
 .then((res) => res.json())
 .then((data) => {
   console.log(data);
 
-  var arr = data.data.results.map(_ => _.name);
-  const shuffledQuestions = arr.sort(() => Math.random() - .5)
+  var arr = data.data.results.map(_ => {return{marvelName:_.name, marvelImage:_.thumbnail}});
+  let realArray = arr.filter(function(marvelElement) {
+    if (marvelElement.marvelImage.path.includes("image_not_available")){
+      return false;
+    } else {
+    return true;
+  }})
+
+  const shuffledQuestions = realArray.sort(() => Math.random() - .5)
 
   let index = 0;
   // let buttonOne = 2;
@@ -39,9 +46,9 @@ fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${
       alert("right answer");
     } else {
       alert("wrong answer");
-      { window.location.reload() }
+        } { window.location.reload() }
     }
-  }
+  
 
   function optionTwo() {
 
@@ -51,7 +58,7 @@ fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${
       alert("wrong answer");
       }  { window.location.reload() }
     }
-  
+
 
   function optionThree() {
     if (btnThree.textContent == characterName.textContent) {
@@ -72,25 +79,29 @@ fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${
 
 
   const characterName = document.getElementById('character-name');
-  characterName.textContent = answers[Math.floor(Math.random()*4)];
-  var marvelPic = data.data.results[1].thumbnail.path + "/portrait_uncanny.jpg";
+  var indexNumber = Math.floor(Math.random()*4);
+  console.log(indexNumber);
+  characterName.textContent = answers[indexNumber].marvelName;
+  console.log(answers); 
+  var marvelPic = answers[indexNumber].marvelImage.path+ "/portrait_xlarge.jpg" ;
+  console.log(marvelPic);
   
   
 
   const btnOne = document.getElementById("btn-1");
-  btnOne.textContent = answers[0];
+  btnOne.textContent = answers[0].marvelName;
   btnOne.addEventListener('click', optionOne);
 
   const btnTwo = document.getElementById("btn-2");
-  btnTwo.textContent = answers[1];
+  btnTwo.textContent = answers[1].marvelName;
   btnTwo.addEventListener('click', optionTwo);
 
   const btnThree = document.getElementById("btn-3");
-  btnThree.textContent = answers[2];
+  btnThree.textContent = answers[2].marvelName;
   btnThree.addEventListener('click', optionThree);
 
   const btnFour = document.getElementById("btn-4");
-  btnFour.textContent = answers[3];
+  btnFour.textContent = answers[3].marvelName;
   btnFour.addEventListener('click', optionFour);
 
   document.getElementById("marvel-picture").src = marvelPic;
@@ -101,17 +112,3 @@ fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${
     document.getElementById("next-btn").addEventListener('click', nextItem);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
